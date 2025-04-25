42c75e2a0b57033e736a661a1312e78978ce662d
yamada2450 <ttc2450sd0014@edu.tech.ac.jp>
2025-04-25 18:41:29 +0900

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,6 @@
+$(function () {
+    lightbox.option( {
+        'wrapAround':true,
+        'albumLabe1': '%1枚目 / 全%2枚'
+    });
+});
---
+++ js/script.js
@@ -0,0 +1,9 @@
+// document.getElementById("myButton").addEventListener("click",function() {
+//     alert("ボタンを押しましたよ")
+// });
+
+$(function() {
+    $("#myButton").click(function () {
+        alert("ボタンを押しました");
+    })
+});
